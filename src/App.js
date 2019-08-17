2609407c096597a7b33712fb4ab29d5a666543b7
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-08-17 12:12:41 -0400

    implemented firebase utils to support user authentication

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import ShopPage from "./pages/shop/shop";
 import { Route, Switch } from "react-router-dom";
 import Header from "./components/header/header";
 import Authentication from './pages/authentication/authentication';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,9 +19,23 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
+        console.log(this.state);
+      }
+
+      // set state on user sign out
+      this.setState( {currentUser: userAuth });
     });
   }
 
--- src/pages/authentication/authentication.jsx
+++ src/pages/authentication/authentication.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./authentication.scss";
 import SignIn from "../../components/sign-in/sign-in";
+import SignUp from "../../components/sign-up/sign-up";
 
 const Authentication = () => {
   return (
     <div className="authentication">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
