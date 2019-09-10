da9fc8b62f1127d4c082880f03d6ef5f7f5e4f41
ChrisZhang95 <chris.zhang95@hotmail.com>
2019-09-09 21:43:46 -0400

    added Strip API for payment

--- src/pages/checkout/checkout.jsx
+++ src/pages/checkout/checkout.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import { connect } from 'react-redux';
-import { selectCartItems, selectCartTotal }from '../../redux/cart/cart.selector.js';
-import { createStructuredSelector } from 'reselect';
+import { connect } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal
+} from "../../redux/cart/cart.selector.js";
+import { createStructuredSelector } from "reselect";
 
-
-
-import './checkout.scss';
-import CheckoutItem from '../../components/checkout-item/checkout-item.jsx';
+import "./checkout.scss";
+import CheckoutItem from "../../components/checkout-item/checkout-item.jsx";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button";
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -34,6 +36,11 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      *Please use the following test credit card for payments* <br />
+      4242 4242 4242 4242 - Exp 01/20 - CVV: 123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
